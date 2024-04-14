54beeb9a1cfa34d57200d5c3e79cbe0d6d751035
TheDnni03 <DavVen_11@outlook.es>
2024-04-14 14:43:49 -0600

    Interfaz mas bonita

--- Nuevos Usuarios.py
+++ Nuevos Usuarios.py
@@ -1,6 +1,8 @@
 import tkinter as tk
+from tkinter import ttk
 from tkinter import messagebox
 import psycopg2
+from ttkthemes import ThemedStyle
 
 def insertar_datos():
     nombre = entry_nombre.get()
@@ -24,24 +26,41 @@ def insertar_datos():
     except (Exception, psycopg2.DatabaseError) as error:
         messagebox.showerror("Error", "Error al insertar: {}".format(error))
 
+# Función para centrar la ventana
+def center_window(window):
+    window.update_idletasks()
+    width = window.winfo_width()
+    height = window.winfo_height()
+    x = (window.winfo_screenwidth() // 2) - (300 // 2)  # Centramos respecto al ancho deseado (300)
+    y = (window.winfo_screenheight() // 2) - (300 // 2)  # Centramos respecto al alto deseado (300)
+    window.geometry('300x300+{}+{}'.format(x, y))
+
 # Crear ventana
 ventana = tk.Tk()
 ventana.title("Interfaz de Inserción de Datos")
+ventana.geometry('300x300')  # Establecer el tamaño de la ventana
+
+# Cargar el estilo de Plastik
+style = ThemedStyle(ventana)
+style.set_theme("plastik")
 
 # Etiquetas y campos de entrada
-label_nombre = tk.Label(ventana, text="Nombre:")
+label_nombre = ttk.Label(ventana, text="Nombre:")
 label_nombre.grid(row=0, column=0, padx=10, pady=5, sticky=tk.E)
-entry_nombre = tk.Entry(ventana)
+entry_nombre = ttk.Entry(ventana)
 entry_nombre.grid(row=0, column=1, padx=10, pady=5)
 
-label_password = tk.Label(ventana, text="Contraseña:")
+label_password = ttk.Label(ventana, text="Contraseña:")
 label_password.grid(row=1, column=0, padx=10, pady=5, sticky=tk.E)
-entry_password = tk.Entry(ventana, show="*")
+entry_password = ttk.Entry(ventana, show="*")
 entry_password.grid(row=1, column=1, padx=10, pady=5)
 
 # Botón de inserción
-btn_insertar = tk.Button(ventana, text="Insertar Datos", command=insertar_datos)
+btn_insertar = ttk.Button(ventana, text="Insertar Datos", command=insertar_datos)
 btn_insertar.grid(row=2, columnspan=2, padx=10, pady=10)
+
+# Centrar ventana
+center_window(ventana)
 
 # Ejecutar ventana
 ventana.mainloop()
